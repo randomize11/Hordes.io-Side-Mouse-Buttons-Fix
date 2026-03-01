421ad099362eace93f7c744c9c0ae1d5d9621bc4
randomize11 <randompro10101@gmail.com>
2026-02-28 16:41:40 -0800

    Update mouseMod.js

--- mouseMod.js
+++ mouseMod.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Hordes.io Side Mouse Buttons Fix
 // @match        https://hordes.io/play
-// @description  Forces standard side mouse buttons (m3/m4) to always apply on in-game canvas.
+// @description  Forces standard side mouse buttons (m3/m4) to always apply to in-game canvas.
 // @author       Randomize
 // @grant        none
 // ==/UserScript==
 
 /* Info:
-To use the side mouse buttons in Hordes.io, use "m3"/"m4" as an in-game key binding. "m3" refers to the lower side button "back", and "m4" refers to the upper side button "forwards".
+To use the side mouse buttons in Hordes.io, use "m3" or "m4" as an in-game key binding. "m3" refers to the lower side button "back", and "m4" refers to the upper side button "forward".
 This mod does not work on Firefox or any Gecko-based browsers. It works in Chrome. Other Chromium-based browsers have not been tested.
 This mod only works with the standard 2 side buttons and does not support additional side buttons.
 */
